a999516f6f1c3624b1b023730a6fe9ddc8cc0367
3punk <112157084+3punk@users.noreply.github.com>
2023-03-14 08:14:23 +0700

    Add files via upload

---
+++ script/vanta.js
@@ -0,0 +1,15 @@
+VANTA.NET({
+    el: "#net",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color: 0xfffc,
+    backgroundColor: 0x19193b,
+    points: 13.00,
+    maxDistance: 21.00,
+    spacing: 16.00
+  })
